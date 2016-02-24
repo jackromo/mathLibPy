b625556ef0d6c3470ff17ff23cb69621668caeb0
Jack Romo <sharrackor@gmail.com>
2016-02-24 01:54:34 +0000

    Add __str__() method

--- polynomial.py
+++ polynomial.py
@@ -5,7 +5,7 @@ class Polynomial(object):
     def __init__(self, coeffs):
         """
         1 parameter:
-        coeff (list): coeff[n] = coefficient of nth degree term
+        coeffs (list): coeffs[n] = coefficient of nth degree term
         """
         self.coeffs = coeffs
 
@@ -26,13 +26,27 @@ class Polynomial(object):
         else:
             self._coeffs = c
 
+    def __str__(self):
+        result = ""
+        for i, c in enumerate(self.coeffs):
+            if i == 0:
+                result += str(c)
+            elif c == 0:
+                continue
+            elif c < 0:
+                result += " - {0}x^{1}".format(-c, i)
+            else:
+                result += " + {0}x^{1}".format(c, i)
+        return result
+
 
 def main():
-    p1 = Polynomial([1]) # p = 1
+    p1 = Polynomial([1])        # p = 1
     p2 = Polynomial([2, 3, 4])  # p = 2 + 3x + 4x^2
     assert(p1.degree == 0)
     assert(p2.degree == 2)
     assert(p2.coeffs == [2, 3, 4])
+    assert(p2.__str__() == "2 + 3x^1 + 4x^2")
 
 if __name__ == "__main__":
     main()
