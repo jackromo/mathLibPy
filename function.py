33a8b6209dd1087f8aad164765be2aea3b5ecba9
Jack Romo <sharrackor@gmail.com>
2016-02-25 20:35:50 +0000

    Move arithmetic and composition functionality into Function class

--- function.py
+++ function.py
@@ -2,63 +2,24 @@ import abc
 import numbers
 
 
-class Function(object, abc.ABCMeta):
+class Function(object):
     """
     Abstract class for all functions composed of elementary cts diff'ble ones.
     Functions are composed of parse trees, with leaf nodes being elementary functions.
     The tree includes arithmetic and compositional combinations of inner functions.
     """
 
-    @abc.abstractmethod
-    def __call__(self, x):
-        """
-        If x is a number, take input x and return f(x).
-        If x is a function, compose self with x.
-        """
-
-    @abc.abstractmethod
-    def __add__(self, other):
-        """Add self to another function"""
-
-    @abc.abstractmethod
-    def __sub__(self, other):
-        """Subtract self from another function"""
-
-    @abc.abstractmethod
-    def __mul__(self, other):
-        """Multiply self with another function"""
-
-    @abc.abstractmethod
-    def __div__(self, other):
-        """Divide self with another function"""
-
-    def _compose(self, other):
-        """
-        Compose self with another function.
-        Cannot be done differently by another function, so implemented here.
-        """
-        if not isinstance(other, Function):
-            raise TypeError("Can only compose Function with Function")
-        return FunctionCompNode(self, other)
-
-
-class FunctionBinaryTreeNode(Function, abc.ABCMeta):
-    """
-    Abstract node of function parse tree for combination of two functions.
-    Concrete subclasses are functions themselves, and can be evaluated.
-    """
-
-    def __init__(self, f1, f2):
-        if not (isinstance(f1, Function) and isinstance(f2, Function)):
-            raise TypeError("Both inputs must be of type Function")
-        self.f1 = f1
-        self.f2 = f2
+    __metaclass__ = abc.ABCMeta
 
     def __call__(self, x):
+        """
+        If x is a number, return f(x).
+        If x is a function, return self composed with x.
+        """
         if isinstance(x, numbers.Number):
             return self._evaluate(x)
         elif isinstance(x, Function):
-            return self._compose(x)
+            return FunctionCompNode(self, x)
         else:
             raise TypeError("Can only be called on Function or Number")
 
@@ -88,6 +49,21 @@ class FunctionBinaryTreeNode(Function, abc.ABCMeta):
         if not isinstance(other, Function):
             raise TypeError("Other must be of type Function")
         return FunctionDivNode(self, other)
+
+
+class FunctionBinaryTreeNode(Function):
+    """
+    Abstract node of function parse tree for combination of two functions.
+    Concrete subclasses are functions themselves, and can be evaluated.
+    """
+
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, f1, f2):
+        if not (isinstance(f1, Function) and isinstance(f2, Function)):
+            raise TypeError("Both inputs must be of type Function")
+        self.f1 = f1
+        self.f2 = f2
 
 
 class FunctionAddNode(FunctionBinaryTreeNode):
