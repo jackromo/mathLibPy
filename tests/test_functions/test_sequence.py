3f636399b12622a4b077f917a4fa924da938b097
Jack Romo <sharrackor@gmail.com>
2016-03-01 04:33:12 +0000

    Separate Sequence tests into separate tests per class

--- tests/test_functions/test_sequence.py
+++ tests/test_functions/test_sequence.py
@@ -4,24 +4,40 @@ import unittest
 
 class SequenceTester(unittest.TestCase):
 
+    def test_abstract(self):
+        self.assertRaises(Exception, Sequence.__init__)
+
+
+class ArithmeticSequenceTester(unittest.TestCase):
+
     def setUp(self):
         self.arith_seq = ArithmeticSequence(1, 1)
-        self.geom_seq = GeometricSequence(1, 2)
-        self.const_seq = FunctionSequence(Constant(1))
 
-    def test_arith_seq_eq(self):
+    def test_eq(self):
         self.assertEqual(self.arith_seq, ArithmeticSequence(1, 1))
 
-    def test_arith_seq_call(self):
+    def test_call(self):
         self.assertEqual(self.arith_seq(0), 1)
         self.assertEqual(self.arith_seq(2), 3)
 
-    def test_geom_seq_eq(self):
+
+class GeometricSequenceTester(unittest.TestCase):
+
+    def setUp(self):
+        self.geom_seq = GeometricSequence(1, 2)
+
+    def test_eq(self):
         self.assertEqual(self.geom_seq, GeometricSequence(1, 2))
 
-    def test_geom_seq_call(self):
+    def test_call(self):
         self.assertEqual(self.geom_seq(0), 1)
         self.assertEqual(self.geom_seq(3), 8)
+
+
+class FunctionSequenceTester(unittest.TestCase):
+
+    def setUp(self):
+        self.const_seq = FunctionSequence(Constant(1))
 
     def test_func_seq_eq(self):
         self.assertEqual(self.const_seq, FunctionSequence(Constant(1)))
@@ -29,6 +45,7 @@ class SequenceTester(unittest.TestCase):
     def test_func_seq_call(self):
         self.assertEqual(self.const_seq(0), 1)
         self.assertEqual(self.const_seq(10), 1)
+
 
 if __name__ == "__main__":
     unittest.main()
