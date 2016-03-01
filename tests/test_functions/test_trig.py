3d316a03ba0c6db9fa35b1979cc8ec2910971830
Jack Romo <sharrackor@gmail.com>
2016-03-01 14:00:26 +0000

    Separate Trig tests into separate tests per class

--- tests/test_functions/test_trig.py
+++ tests/test_functions/test_trig.py
@@ -2,39 +2,50 @@ from mathlibpy.functions import *
 import unittest
 
 
-class TrigTester(unittest.TestCase):
+class SinTester(unittest.TestCase):
 
     def setUp(self):
         self.sin = Sin()
-        self.cos = Cos()
-        self.tan = Tan()
 
-    def test_sin_call(self):
+    def test_call(self):
         self.assertEqual(self.sin(0), 0)
 
-    def test_sin_eq(self):
+    def test_eq(self):
         self.assertEqual(self.sin, Sin())
 
-    def test_cos_call(self):
-        self.assertEqual(self.cos(0), 1)
-
-    def test_cos_eq(self):
-        self.assertEqual(self.cos, Cos())
-
-    def test_tan_call(self):
-        self.assertEqual(self.tan(0), 0)
-
-    def test_tan_eq(self):
-        self.assertEqual(self.tan, Tan())
-
-    def test_add_call(self):
-        self.assertEqual((self.tan + self.cos + self.sin)(0), 1)
-
-    def test_sin_get_derivative_call(self):
+    def test_get_derivative_call(self):
         self.assertEqual(self.sin.get_derivative()(0), 1)
 
-    def test_cos_get_derivative_call(self):
+
+class CosTester(unittest.TestCase):
+
+    def setUp(self):
+        self.cos = Cos()
+
+    def test_call(self):
+        self.assertEqual(self.cos(0), 1)
+
+    def test_eq(self):
+        self.assertEqual(self.cos, Cos())
+
+    def test_get_derivative_call(self):
         self.assertEqual(self.cos.get_derivative()(math.pi/2), -1)
+
+
+class TanTester(unittest.TestCase):
+
+    def setUp(self):
+        self.tan = Tan()
+
+    def test_call(self):
+        self.assertEqual(self.tan(0), 0)
+
+    def test_eq(self):
+        self.assertEqual(self.tan, Tan())
+
+    def test_get_derivative(self):
+        self.assertEqual(self.tan.get_derivative()(0), 1)
+
 
 if __name__ == "__main__":
     unittest.main()
