b4f5b5da5e7a7266e7f908b6ffc975ea3f1f0657
Jack Romo <sharrackor@gmail.com>
2016-03-01 02:25:55 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='MathLibPy',
+      version='0.0.0',
+      description='Math library for Python',
+      author='Jack Romo',
+      author_email='sharrackor@gmail.com',
+      packages=['mathlibpy'],
+      )
