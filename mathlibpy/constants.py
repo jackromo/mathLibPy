eefda8baf4a1c3ee78ad1a58eed99bbfb1f3049f
Jack Romo <sharrackor@gmail.com>
2016-03-02 20:38:03 +0000

    Add constants for infinities and undefined values

---
+++ mathlibpy/constants.py
@@ -0,0 +1,9 @@
+# All universal constants in MathLibPy.
+# TODO: Make these actually constant.
+
+INFINITY = "infinity"
+NEG_INF = "neg_inf"     # Negative infinity
+UNDEFINED = "undefined"
+
+REAL_CARD = "real_card" # Cardinality of the set of real numbers
+NAT_CARD = "nat_card"   # Cardinality of the set of natural numbers
