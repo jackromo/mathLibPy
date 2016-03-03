b6eb0795bb9089c2e4c296d92c7e21a3236e69b4
Jack Romo <sharrackor@gmail.com>
2016-03-03 00:34:33 +0000

    Add tests for all SetPyTreeNode subclasses

--- tests/discrete/test_sets.py
+++ tests/discrete/test_sets.py
@@ -8,6 +8,12 @@ class SetPyTester(unittest.TestCase):
         self.assertRaises(Exception, SetPy.__init__)
 
 
+class SetPyTreeNodeTester(unittest.TestCase):
+
+    def test_abstract(self):
+        self.assertRaises(Exception, SetPyTreeNode.__init__)
+
+
 class FiniteSetPyTester(unittest.TestCase):
 
     def setUp(self):
@@ -48,6 +54,123 @@ class FiniteSetPyTester(unittest.TestCase):
     def test_is_proper_subset(self):
         self.assertFalse(self.set3.is_proper_subset(self.set2))
         self.assertTrue(self.set2.is_proper_subset(self.set3))
+
+
+class SetPyUnionNodeTester(unittest.TestCase):
+
+    def setUp(self):
+        self.set1 = SetPyUnionNode(FiniteSetPy([1]), FiniteSetPy([2]))
+        self.set2 = SetPyUnionNode(FiniteSetPy([3]), FiniteSetPy([3]))
+
+    def test_contains(self):
+        self.assertTrue(1 in self.set1)
+        self.assertTrue(2 in self.set1)
+        self.assertTrue(3 in self.set2)
+
+    def test_cardinality(self):
+        self.assertEqual(self.set1.cardinality(), 2)
+        self.assertEqual(self.set2.cardinality(), 1)
+
+    def test_is_finite(self):
+        self.assertTrue(self.set1.is_finite())
+        self.assertTrue(self.set2.is_finite())
+
+    def test_elems(self):
+        self.assertTrue(all(x in self.set1 for x in self.set1.elems()))
+        self.assertEquals(self.set2.elems(), [3])
+
+    def test_eq(self):
+        self.assertEqual(self.set1, FiniteSetPy([1, 2]))
+        self.assertEqual(self.set2, FiniteSetPy([3]))
+
+    def test_subset(self):
+        self.assertTrue(self.set1.is_subset(FiniteSetPy([1, 2])))
+        self.assertFalse(self.set1.is_subset(FiniteSetPy([1])))
+
+    def test_proper_subset(self):
+        self.assertTrue(self.set1.is_proper_subset(FiniteSetPy([1, 2, 3, 4])))
+        self.assertFalse(self.set1.is_proper_subset(self.set1))
+        self.assertFalse(self.set1.is_proper_subset(FiniteSetPy()))
+
+
+class SetPyIntersectNodeTester(unittest.TestCase):
+
+    def setUp(self):
+        self.set1 = SetPyIntersectNode(FiniteSetPy([1, 2, 3]), FiniteSetPy([2, 3, 4]))
+        self.set2 = SetPyIntersectNode(FiniteSetPy([3]), FiniteSetPy([4, 3]))
+
+    def test_contains(self):
+        self.assertFalse(1 in self.set1)
+        self.assertTrue(2 in self.set1)
+        self.assertTrue(3 in self.set1)
+        self.assertTrue(3 in self.set2)
+        self.assertFalse(4 in self.set2)
+
+    def test_cardinality(self):
+        self.assertEqual(self.set1.cardinality(), 2)
+        self.assertEqual(self.set2.cardinality(), 1)
+
+    def test_is_finite(self):
+        self.assertTrue(self.set1.is_finite())
+        self.assertTrue(self.set2.is_finite())
+
+    def test_elems(self):
+        self.assertTrue(all(x in self.set1 for x in self.set1.elems()))
+        self.assertEquals(self.set2.elems(), [3])
+
+    def test_eq(self):
+        self.assertEqual(self.set1, FiniteSetPy([2, 3]))
+        self.assertEqual(self.set2, FiniteSetPy([3]))
+
+    def test_subset(self):
+        self.assertTrue(self.set1.is_subset(FiniteSetPy([1, 2, 3])))
+        self.assertTrue(self.set1.is_subset(self.set1))
+        self.assertFalse(self.set1.is_subset(FiniteSetPy([1])))
+
+    def test_proper_subset(self):
+        self.assertTrue(self.set1.is_proper_subset(FiniteSetPy([1, 2, 3])))
+        self.assertFalse(self.set1.is_proper_subset(self.set1))
+        self.assertFalse(self.set1.is_proper_subset(FiniteSetPy()))
+
+
+class SetPyDifferenceNodeTester(unittest.TestCase):
+
+    def setUp(self):
+        self.set1 = SetPyDifferenceNode(FiniteSetPy([1, 2, 3, 4]), FiniteSetPy([3, 4]))
+        self.set2 = SetPyDifferenceNode(FiniteSetPy([3]), FiniteSetPy([4]))
+
+    def test_contains(self):
+        self.assertTrue(1 in self.set1)
+        self.assertTrue(2 in self.set1)
+        self.assertFalse(4 in self.set1)
+        self.assertTrue(3 in self.set2)
+        self.assertFalse(4 in self.set2)
+
+    def test_cardinality(self):
+        self.assertEqual(self.set1.cardinality(), 2)
+        self.assertEqual(self.set2.cardinality(), 1)
+
+    def test_is_finite(self):
+        self.assertTrue(self.set1.is_finite())
+        self.assertTrue(self.set2.is_finite())
+
+    def test_elems(self):
+        self.assertTrue(all(x in self.set1 for x in self.set1.elems()))
+        self.assertEquals(self.set2.elems(), [3])
+
+    def test_eq(self):
+        self.assertEqual(self.set1, FiniteSetPy([1, 2]))
+        self.assertEqual(self.set2, FiniteSetPy([3]))
+
+    def test_subset(self):
+        self.assertTrue(self.set1.is_subset(FiniteSetPy([1, 2, 3])))
+        self.assertTrue(self.set1.is_subset(self.set1))
+        self.assertFalse(self.set1.is_subset(FiniteSetPy([1])))
+
+    def test_proper_subset(self):
+        self.assertTrue(self.set1.is_proper_subset(FiniteSetPy([1, 2, 3])))
+        self.assertFalse(self.set1.is_proper_subset(self.set1))
+        self.assertFalse(self.set1.is_proper_subset(FiniteSetPy()))
 
 
 if __name__ == "__main__":
