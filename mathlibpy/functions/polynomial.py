74debed2a4df5898a1a30bad6b796a99e55d0366
Jack Romo <sharrackor@gmail.com>
2016-03-13 00:44:07 +0000

    Add docstrings to Polynomial

--- mathlibpy/functions/polynomial.py
+++ mathlibpy/functions/polynomial.py
@@ -2,11 +2,14 @@ import function
 
 
 class Polynomial(function.Function):
+    """
+    Generic dense one-variable polynomial.
+    """
 
     def __init__(self, coeffs):
         """
-        1 parameter:
-        coeffs (list): coeffs[n] = coefficient of nth degree term
+        @type coeffs: list
+        @param coeffs: List of all coefficients, indexed by degree. ie. coeffs[n] = nth degree coefficient.
         """
         self.coeffs = coeffs
 
@@ -34,6 +37,9 @@ class Polynomial(function.Function):
         return sum(self[i]*(x**i) for i in range(self.degree+1))
 
     def __str__(self):
+        """
+        @return: String of following form: '#x^0 +- #x^1 +- ... +- #x^n'
+        """
         result = ""
         for i, c in enumerate(self.coeffs):
             if i == 0:
@@ -55,6 +61,13 @@ class Polynomial(function.Function):
             return all(self[i] == other[i] for i in range(self.degree))
 
     def __getitem__(self, item):
+        """
+        Gets nth coefficient of polynomial.
+
+        @type item: int
+        @param item: Degree of the term whose coefficient will be retrieved.
+        @return: Coefficient of term with degree equal to provided index.
+        """
         if not isinstance(item, int):
             raise TypeError("Tried to index with a non-integer")
         elif item < 0:
@@ -65,6 +78,15 @@ class Polynomial(function.Function):
             return self.coeffs[item]
 
     def __setitem__(self, key, value):
+        """
+        Resets nth coefficient of polynomial to prescribed value.
+
+        @type key: int
+        @param key: Degree of the term whose coefficient will be altered.
+        @type value: number
+        @param value: New coefficient value.
+        @return: Nothing.
+        """
         if not isinstance(key, int):
             raise TypeError("Tried to index with a non-integer")
         elif key < 0:
