c3a7c4f6b6632b9ac620ceefb08ba82dc6a63fbe
Jack Romo <sharrackor@gmail.com>
2016-03-13 15:35:20 +0100

    Add docstrings to Infinity

--- mathlibpy/constants/infinity.py
+++ mathlibpy/constants/infinity.py
@@ -2,8 +2,22 @@ import numbers
 
 
 class Infinity(numbers.Number):
+    """
+    An infinite number. Infinite numbers can be used in arithmetic and comparisons.
+    Infinities are comparable with each other, and are considered numbers.
+
+    @note: Infinities are not technically numbers, as they disobey arithmetic laws.
+    For simplicity and consistency, they are considered to be numbers here.
+    """
 
     def __init__(self, rank, grp_id):
+        """
+        @type rank: int
+        @param rank: Defines ordering of infinity within its ordering group. Comparison compares ranks of infinities.
+        If rank is negative, then infinity is also negative.
+        @type grp_id: int
+        @param grp_id: ID of group of infinities that self can be meaningfully compared with.
+        """
         self.rank = rank
         self.grp_id = grp_id
 
@@ -32,6 +46,10 @@ class Infinity(numbers.Number):
         return Infinity(-self.rank, self.grp_id)
 
     def __add__(self, other):
+        """
+        @type other: number
+        @return: If other is an Infinity, return max of self and other. Otherwise, return self.
+        """
         if isinstance(other, Infinity):
             if other.grp_id == self.grp_id:
                 return max(self, other)
@@ -43,12 +61,21 @@ class Infinity(numbers.Number):
             raise TypeError("Not composable with non-number")
 
     def __sub__(self, other):
+        """
+        @type other: number
+        @return: If other is a number, return self.
+        """
         if isinstance(other, numbers.Number):
             return self
         else:
             raise TypeError("Not composable with non-number")
 
     def __mul__(self, other):
+        """
+        @type other: number
+        @return: If other is an infinity, return negative self if other has opposite sign, else positive self.
+        Otherwise, return 0 if other is 0, or -self if other's sign is opposite to self.
+        """
         if isinstance(other, Infinity):
             return Infinity(self.rank * (other.rank / float(abs(other.rank))), self.grp_id)
         elif isinstance(other, numbers.Number):
@@ -62,6 +89,11 @@ class Infinity(numbers.Number):
             raise TypeError("Not composable with non-number")
 
     def __div__(self, other):
+        """
+        @type other: number
+        @return: Self with opposite sign if own sign different from other sign.
+        @raise ZeroDivisionError: Other is 0.
+        """
         if isinstance(other, Infinity):
             return Infinity(self.rank * (other.rank / float(abs(other.rank))), self.grp_id)
         elif isinstance(other, numbers.Number):
