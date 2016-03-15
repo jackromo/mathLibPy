9484e8d41faf52c46804b98d8eca074345c1ee09
Jack Romo <sharrackor@gmail.com>
2016-03-15 01:30:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='MathLibPy',
-      version='0.0.0',
-      description='Math library for Python',
-      author='Jack Romo',
-      author_email='sharrackor@gmail.com',
-      packages=['mathlibpy'],
-      )
+setup(
+    name="MathLibPy",
+    version="0.0.0",
+    url="http://github.com/jackromo/mathLibPy",
+    license="MIT",
+    description="Math library for Python",
+    author="Jack Romo",
+    author_email="sharrackor@gmail.com",
+    platforms="any",
+    packages=['mathlibpy'],
+    install_requires=[
+        "sphinx>=1.3.6"
+    ],
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python 2.7"
+    ]
+)
