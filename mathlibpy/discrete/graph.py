cffebd129310e8ce1cdd6b47df10e1fc0cce3c14
Jack Romo <sharrackor@gmail.com>
2016-03-24 23:18:53 +0100

    Add template for Graph class

---
+++ mathlibpy/discrete/graph.py
@@ -0,0 +1,48 @@
+
+
+class Graph(object):
+    """
+    Undirected graph with weightless edges.
+    """
+
+    def __init__(self, nodes, edges):
+        pass
+
+    @property
+    def nodes(self):
+        pass
+
+    @property
+    def edges(self):
+        pass
+
+    @property
+    def adj_matrix(self):
+        pass
+
+    def is_tree(self):
+        pass
+
+    def __eq__(self, other):
+        """
+        Isomorphism test.
+        """
+        pass
+
+    def add_node(self):
+        pass
+
+    def remove_node(self):
+        pass
+
+    def add_edge(self):
+        pass
+
+    def remove_edge(self):
+        pass
+
+    def get_adjacent_nodes(self, node):
+        pass
+
+    def get_min_spanning_tree(self):
+        pass
---
+++ tests/test_discrete/test_graph.py
@@ -0,0 +1,8 @@
+from mathlibpy.discrete import *
+import unittest
+
+
+class GraphTester(unittest.TestCase):
+
+    def setUp(self):
+        pass
