20e87256fe25bf61f41e3254f955b5d9ee8a58e9
Jack Romo <sharrackor@gmail.com>
2016-05-26 23:40:22 +0100

    Improve matrix __init__ and indexing

--- mathlibpy/matrices/matrix.py
+++ mathlibpy/matrices/matrix.py
@@ -13,75 +13,45 @@ class Matrix(object):
     Generic m*n matrix.
     """
 
-    def __init__(self, rows, cols, body=None):
+    def __init__(self, body=None, rows=1, cols=1):
         """
-        Notes:
-            Provision of number of rows and columns will soon be deprecated.
-
         Args:
-            rows (int): Number of rows in matrix.
-            cols (int): Number of columns in matrix.
-            body (list[list]): Matrix body as list of lists, each list being a matrix row.
+            rows (int): Number of rows in matrix if None.
+            cols (int): Number of columns in matrix if None.
+            body (list[list], None): Matrix body as list of lists, each list being a matrix row.
         """
-        self.rows = rows
-        self.cols = cols
         if not body:
             # Default is all 0's
-            self.body = [[0 for _ in range(cols)] for _ in range(rows)]
+            self._body = [[0 for _ in range(cols)] for _ in range(rows)]
         else:
             if not isinstance(body, list):
                 raise TypeError("body must be list row lists")
             elif not all(isinstance(body[i], list) for i in range(len(body))):
                 raise TypeError("elements of body must be lists")
-            elif rows != len(body):
-                raise ValueError("number of rows supplied inconsistent with body")
-            elif not all(cols == len(body[i]) for i in range(self.rows)):
-                raise ValueError("body columns inconsistent with supplied value")
             else:
-                self.body = body
+                self._body = body
+        self._rows = len(self._body)
+        self._cols = len(self._body[0])
 
     @property
     def rows(self):
         return self._rows
 
-    @rows.setter
-    def rows(self, r):
-        assert(isinstance(r, int))
-        self._rows = r
-
     @property
     def cols(self):
         return self._cols
 
-    @cols.setter
-    def cols(self, c):
-        assert(isinstance(c, int))
-        self._cols = c
+    def __getitem__(self, key):
+        """
+        Index matrix as mat[x, y].
 
-    @property
-    def body(self):
+        Args:
+            key (tuple(int, int)): Pair of x-y coordinates in matrix; x is column, y is row. 0-indexed.
         """
-        Notes:
-            This will soon be deprecated in favor of __getitem__ on tuples.
-        """
-        return self._body
+        return self._body[key[1]][key[0]]
 
-    @body.setter
-    def body(self, b):
-        """
-        Notes:
-            This will soon be deprecated in favor of __setitem__ on tuples.
-        """
-        if not isinstance(b, list):
-            raise TypeError("body must be a list")
-        elif not all(isinstance(b[i], list) for i in range(len(b))):
-            raise TypeError("elements of body must be lists")
-        elif not all(len(b[0]) == len(b[i]) for i in range(len(b))):
-            raise TypeError("all rows must be of same length")
-        else:
-            self.rows = len(b)
-            self.cols = len(b[0])
-            self._body = b
+    def __setitem__(self, key, value):
+        self._body[key[1]][key[0]] = value
 
     def __str__(self):
         """
@@ -91,7 +61,7 @@ class Matrix(object):
         [elem21 elem22 elem23 ...]
         ...
         """
-        return "\n".join("[" + " ".join(str(i) for i in row) + "]" for row in self.body)
+        return "\n".join("[" + " ".join(str(i) for i in row) + "]" for row in self._body)
 
     def __eq__(self, other):
         if not isinstance(other, Matrix):
@@ -99,21 +69,20 @@ class Matrix(object):
         elif (self.rows != other.rows) or (self.cols != other.cols):
             return False
         else:
-            return all(self.body[r][c] == other.body[r][c] for r in range(self.rows) for c in range(self.cols))
+            return all(self[x, y] == other[x, y] for y in range(self.rows) for x in range(self.cols))
 
     def __add__(self, other):
         if not isinstance(other, Matrix):
             raise TypeError("Cannot add non-matrix to matrix")
         elif self.rows != other.rows or self.cols != other.cols:
             raise ValueError("Matrices do not have same dimensions")
-        return Matrix(self.rows, self.cols, [[self.body[r][c]+other.body[r][c] for c in range(self.cols)]
-                                             for r in range(self.rows)])
+        return Matrix([[self[x, y] + other[x, y] for x in range(self.cols)] for y in range(self.rows)])
 
     def __mul__(self, other):
         """
         Can multiply with scalar or Matrix.
         """
-        if isinstance(other, type(self.body[0][0])):
+        if isinstance(other, type(self[0, 0])):
             return self._mul_scalar(other)
         elif isinstance(other, Matrix):
             return self._mul_matrix(other)
@@ -131,27 +100,21 @@ class Matrix(object):
         Returns:
             Matrix: The identity matrix of prescribed number of columns.
         """
-        return Matrix(cols, cols, [[1 if r == c else 0 for c in range(cols)]
-                                   for r in range(cols)])
+        return Matrix([[1 if r == c else 0 for c in range(cols)] for r in range(cols)])
 
     def _mul_scalar(self, other):
-        return Matrix(self.rows,
-                      self.cols,
-                      [[self.body[r][c]*other for c in range(self.cols)]
-                       for r in range(self.rows)])
+        return Matrix([[self[x, y]*other for x in range(self.cols)] for y in range(self.rows)])
 
     def _mul_matrix(self, other):
         # Assume that 'other' is a matrix, as tested in __mul__
         if self.cols != other.rows:
             raise ValueError("Matrix 1 must have same number of columns as rows of matrix 2")
-        m1 = self.body
-        m2 = other.body
         result = []
         for r in range(self.rows):
             result.append([])
             for c in range(other.cols):
-                result[r].append(sum([m1[r][i]*m2[i][c] for i in range(self.cols)]))
-        return Matrix(self.rows, other.cols, result)
+                result[r].append(sum([self[i, r]*other[c, i] for i in range(self.cols)]))
+        return Matrix(result)
 
     def __sub__(self, other):
         if not isinstance(other, Matrix):
@@ -174,8 +137,8 @@ class Matrix(object):
             raise ValueError("Coordinates must be integers")
         elif not (0 <= i <= (self.rows-1) and 0 <= j <= (self.cols-1)):
             raise ValueError("Must have 0<=row<=m-1 and 0<=col<=n-1")
-        result = [[self.body[r][c] for c in range(self.cols) if c != j] for r in range(self.rows) if r != i]
-        return Matrix(self.rows-1, self.cols-1, result)
+        result = [[self[x, y] for x in range(self.cols) if x != j] for y in range(self.rows) if y != i]
+        return Matrix(result)
 
     def get_determinant(self):
         """
@@ -185,15 +148,15 @@ class Matrix(object):
         if self.rows != self.cols:
             raise ValueError("Cannot take determinant of non-square matrix")
         elif self.rows == 1 and self.cols == 1:
-            return self.body[0][0]
+            return self._body[0][0]
         else:
-            return sum(((-1)**c)*(self.body[0][c]*self.get_cofactor(0, c).get_determinant()) for c in range(self.cols))
+            return sum(((-1)**x)*(self[x, 0]*self.get_cofactor(0, x).get_determinant()) for x in range(self.cols))
 
     def transpose(self):
         """
         Transpose matrix in place.
         """
-        self.body = [[self.body[c][r] for c in self.cols] for r in self.rows]
+        self._body = [[self[y, x] for x in self.cols] for y in self.rows]
 
     def is_invertible(self):
         """
@@ -212,23 +175,21 @@ class Matrix(object):
         pivot_col = 0
         # Iterate through each column
         for pivot_row in range(self.rows):
-            while result.body[pivot_row][pivot_col] == 0:
+            while result[pivot_col, pivot_row] == 0:
                 # Check all lower rows, swap with one below
                 has_swapped = False
                 for r in range(pivot_row+1, result.rows):
-                    if result.body[r][pivot_col] != 0:
+                    if result[pivot_col, r] != 0:
                         # Found a swappable row, swap values
-                        temp_row = result.body[r]
-                        result.body[r] = result.body[pivot_row]
-                        result.body[pivot_row] = temp_row
+                        result._body[r], result._body[pivot_row] = result._body[pivot_row], result._body[r]
                         has_swapped = True
                 if not has_swapped:
                     pivot_col += 1    # Skip to next column
             # Subtract multiples of current row from all lower rows to make rest of column zero
             for row in range(pivot_row+1, self.rows):
-                factor = result.body[row][pivot_col] / result.body[pivot_row][pivot_col]
-                result.body[row] = [result.body[row][c] - (factor*result.body[pivot_row][c])
-                                    for c in range(result.cols)]
+                factor = result[pivot_col, row] / result[pivot_col, pivot_row]
+                result._body[row] = [result[c, row] - (factor*result[c, pivot_row])
+                                     for c in range(result.cols)]
         return result
 
     def get_reduced_echelon_form(self):
@@ -240,13 +201,13 @@ class Matrix(object):
         # Go up rows in reverse
         for pivot_row in range(self.rows-1, -1, -1):
             # Pivot is first nonzero element in row
-            pivot = next(x for x in result.body[pivot_row] if x != 0)
-            pivot_col = result.body[pivot_row].index(pivot)
+            pivot = next(x for x in result._body[pivot_row] if x != 0)
+            pivot_col = result._body[pivot_row].index(pivot)
             # Subtract multiples of current row from all above rows to make rest of column zero
             for row in range(pivot_row-1, -1, -1):
-                factor = result.body[row][pivot_col] / pivot
-                result.body[row] = [result.body[row][c] - (factor*result.body[pivot_row][c])
-                                    for c in range(result.cols)]
+                factor = result[pivot_col, row] / pivot
+                result._body[row] = [result[c, row] - (factor*result[c, pivot_row])
+                                     for c in range(result.cols)]
         return result
 
     def get_row_reduced_echelon_form(self):
@@ -256,8 +217,8 @@ class Matrix(object):
         """
         result = self.get_reduced_echelon_form()
         for row in range(result.rows):
-            pivot = next(x for x in result.body[row] if x != 0)
-            result.body[row] = [i / pivot for i in result.body[row]]
+            pivot = next(x for x in result._body[row] if x != 0)
+            result._body[row] = [i / pivot for i in result._body[row]]
         return result
 
     def get_inverse(self):
@@ -274,12 +235,12 @@ class Matrix(object):
         ident = Matrix.identity(temp_self.cols)
         # Append identity matrix to right of matrix
         for row in range(temp_self.rows):
-            temp_self.body[row] = temp_self.body[row] + ident.body[row]
+            temp_self._body[row] = temp_self._body[row] + ident._body[row]
         # Unique update of body, must manually reset temp_self.cols
-        temp_self.cols *= 2 # Is square matrix, so number of columns doubles
+        temp_self._cols *= 2     # Is square matrix, so number of columns doubles
         rref_temp_self = temp_self.get_row_reduced_echelon_form()
-        result = Matrix(self.rows, self.cols)
+        result = Matrix(None, self.rows, self.cols)
         # Identity appended on right is now inverse, as left is now identity
         for row in range(result.rows):
-            result.body[row] = rref_temp_self.body[row][self.rows:]
+            result._body[row] = rref_temp_self._body[row][self.rows:]
         return result
--- tests/test_matrices/test_matrix.py
+++ tests/test_matrices/test_matrix.py
@@ -7,22 +7,22 @@ class MatrixTester(unittest.TestCase):
 
     def setUp(self):
         # Generic matrices used for tests
-        self.m1 = Matrix(2, 2)
-        self.m2 = Matrix(2, 2, [[1, 1],
-                                [2, 1]])
+        self.m1 = Matrix(None, 2, 2)
+        self.m2 = Matrix([[1, 1],
+                          [2, 1]])
         self.ident = Matrix.identity(2)
-        self.m3 = Matrix(3, 3, [[1, 0, 4],
-                                [1, 1, 6],
-                                [-3, 0, -10]])
-        self.m4 = Matrix(2, 2, [[0, 1],
-                                [1, 0]])
+        self.m3 = Matrix([[1, 0, 4],
+                          [1, 1, 6],
+                          [-3, 0, -10]])
+        self.m4 = Matrix([[0, 1],
+                          [1, 0]])
 
     def test_str(self):
         self.assertEquals(self.m1.__str__(), "[0 0]\n[0 0]")
 
     def test_eq(self):
-        self.assertTrue(self.m2 == Matrix(2, 2, [[1, 1],
-                                                 [2, 1]]))
+        self.assertTrue(self.m2 == Matrix([[1, 1],
+                                           [2, 1]]))
 
     def test_neq(self):
         self.assertFalse(self.m1 == self.m2)
@@ -31,21 +31,21 @@ class MatrixTester(unittest.TestCase):
         self.assertEqual(self.m1+self.m2, self.m2)
 
     def test_scalar_mult(self):
-        self.assertEqual(self.m2*5, Matrix(2, 2, [[5, 5],
-                                                  [10, 5]]))
+        self.assertEqual(self.m2*5, Matrix([[5, 5],
+                                            [10, 5]]))
 
     def test_mult_ident(self):
         self.assertEqual(self.m2*self.ident, self.m2)
         self.assertEqual(self.ident*self.m2, self.m2)
 
     def test_mult_col_matrix(self):
-        self.assertEqual(self.m2*Matrix(2, 1, [[1],
-                                               [3]]),  Matrix(2, 1, [[4],
-                                                                     [5]]))
+        self.assertEqual(self.m2*Matrix([[1],
+                                         [3]]),  Matrix([[4],
+                                                         [5]]))
 
     def test_cofactor(self):
-        self.assertEqual(self.m3.get_cofactor(0, 0), Matrix(2, 2, [[1, 6],
-                                                                   [0, -10]]))
+        self.assertEqual(self.m3.get_cofactor(0, 0), Matrix([[1, 6],
+                                                             [0, -10]]))
 
     def test_determinant(self):
         self.assertEqual(self.m2.get_determinant(), -1)
@@ -54,23 +54,23 @@ class MatrixTester(unittest.TestCase):
     def test_invertible(self):
         self.assertFalse(self.m1.is_invertible())
         self.assertTrue(self.m2.is_invertible())
-        self.assertFalse(Matrix(1, 2, [[1, 2]]).is_invertible())
+        self.assertFalse(Matrix([[1, 2]]).is_invertible())
 
     def test_get_inverse(self):
-        self.assertEqual(self.m3.get_inverse(), Matrix(3, 3, [[-5, 0, -2],
-                                                              [-4, 1, -1],
-                                                              [1.5, 0, 0.5]]))
+        self.assertEqual(self.m3.get_inverse(), Matrix([[-5, 0, -2],
+                                                        [-4, 1, -1],
+                                                        [1.5, 0, 0.5]]))
         self.assertEqual(self.m4.get_inverse(), self.m4)
 
     def test_get_echelon_form(self):
-        self.assertEqual(self.m3.get_echelon_form(), Matrix(3, 3, [[1, 0, 4],
-                                                                   [0, 1, 2],
-                                                                   [0, 0, 2]]))
+        self.assertEqual(self.m3.get_echelon_form(), Matrix([[1, 0, 4],
+                                                             [0, 1, 2],
+                                                             [0, 0, 2]]))
 
     def test_get_reduced_echelon_form(self):
-        self.assertEqual(self.m3.get_reduced_echelon_form(), Matrix(3, 3, [[1, 0, 0],
-                                                                           [0, 1, 0],
-                                                                           [0, 0, 2]]))
+        self.assertEqual(self.m3.get_reduced_echelon_form(), Matrix([[1, 0, 0],
+                                                                     [0, 1, 0],
+                                                                     [0, 0, 2]]))
 
     def test_get_row_reduced_echelon_form(self):
         self.assertEqual(self.m3.get_row_reduced_echelon_form(), Matrix.identity(3))
